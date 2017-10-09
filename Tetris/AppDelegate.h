f8afc34a78571902692c4b99fa3e826b69e0c294
张欣欣 <zhangxinxin@zhangxinxindeMacBook-Pro.local>
2017-10-09 15:12:00 +0800

    Initial Commit

---
+++ Tetris/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tetris
+//
+//  Created by 张欣欣 on 2017/10/9.
+//  Copyright © 2017年 zxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tetris/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tetris
+//
+//  Created by 张欣欣 on 2017/10/9.
+//  Copyright © 2017年 zxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
